490dda38e12dea0aabb4af10a4a61cf3a6de5891
istahilagi <istahilagi@yahoo.com>
2025-07-05 16:57:11 +0700

    Update apiService.ts

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,5 +1,5 @@
 // API Service untuk integrasi dengan MySQL custom API
-const API_BASE_URL = 'https://man1halteng.alhastream.com/';
+const API_BASE_URL = 'http://45.158.126.50:3002';
 
 export interface ApiResponse<T> {
   success: boolean;
